698a30817656327845df9e8d1ff8a5e0f77a5f7e
Spike Grobstein <spike@ticketevolution.com>
2012-11-07 23:00:37 -0500

    got rules sorta working

--- lib/grepresent.rb
+++ lib/grepresent.rb
@@ -1,5 +1,9 @@
-require "grepresent/version"
 require 'term/ansicolor'
+require 'thor'
+require "grepresent/version"
+require 'grepresent/formatter'
+require 'grepresent/formatter_rule'
+require 'grepresent/cli'
 
 module Grepresent
 end
---
+++ lib/grepresent/cli.rb
@@ -0,0 +1,42 @@
+require 'optparse'
+require 'awesome_print'
+
+module Grepresent
+  class CLI
+
+    attr_reader :rules
+
+    def self.run!
+      # options = []
+      # options << Option.new(:name => [ '-h', '--help' ],
+                            # :)
+      @rules = []
+
+      OptionParser.new do |opts|
+        opts.banner = "Usage: grepresent [ options ]"
+
+        # opts << Option.new(:name => [ '-f', '--format PATTERN FORMAT', [1,1] ],
+                            # :arg_arity => [2,2])
+        opts.on("-f", "--format PATTERN FORMAT", String, String, "Specify a format") do |p|
+          f = ARGV.shift
+          @rules << Grepresent::FormatterRule.new(p, f)
+        end
+
+        opts.on("-h", "--help") do
+          puts opts
+        end
+      end.parse!
+
+    end
+
+    def self.start
+      $stdin.each do |line|
+        @rules.each { |r| line = r.apply(line) }
+        puts line
+      end
+    end
+
+  end
+end
+
+Grepresent::CLI.run!
